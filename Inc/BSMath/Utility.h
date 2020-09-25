c74905d735d2244f054a9ab6fa64328ff90ede93
blAs1N <qazasa123@gmail.com>
2020-09-26 01:38:08 +0900

    Use const reference for vector

--- Inc/BSMath/Utility.h
+++ Inc/BSMath/Utility.h
@@ -8,43 +8,43 @@ namespace BSMath
     constexpr float Epsilon = 1.192092896e-07F;
 
     template <class T1, class T2>
-    [[nodiscard]] constexpr auto Min(T1 lhs, T2 rhs) noexcept
+    [[nodiscard]] constexpr auto Min(const T1& lhs, const T2& rhs) noexcept
     {
         return lhs < rhs ? lhs : rhs;
     }
 
     template <class T1, class T2, class... Ts>
-    [[nodiscard]] constexpr auto Min(T1 x1, T2 x2, Ts... xs) noexcept
+    [[nodiscard]] constexpr auto Min(const T1& x1, const T2& x2, const Ts&... xs) noexcept
     {
         return Min(Min(x1, x2), xs...);
     }
 
     template <class T1, class T2>
-    [[nodiscard]] constexpr auto Max(T1 lhs, T2 rhs) noexcept
+    [[nodiscard]] constexpr auto Max(const T1& lhs, const T2& rhs) noexcept
     {
         return lhs > rhs ? lhs : rhs;
     }
 
     template <class T1, class T2, class... Ts>
-    [[nodiscard]] constexpr auto Max(T1 x1, T2 x2, Ts... xs) noexcept
+    [[nodiscard]] constexpr auto Max(const T1& x1, const T2& x2, const Ts&... xs) noexcept
     {
         return Max(Max(x1, x2), xs...);
     }
 
     template <class T, class U, class V>
-    [[nodiscard]] constexpr auto Clamp(T v, U min, V max) noexcept
+    [[nodiscard]] constexpr auto Clamp(const T& n, const U& min, const V& max) noexcept
     {
-        return Max(Min(v, max), min);
+        return Max(Min(n, max), min);
     }
 
     template <class T>
-    [[nodiscard]] constexpr T Abs(T n) noexcept
+    [[nodiscard]] constexpr T Abs(const T& n) noexcept
     {
         return n >= static_cast<T>(0) ? n : -n;
     }
 
     template <class T>
-    [[nodiscard]] constexpr T Sign(T n) noexcept
+    [[nodiscard]] constexpr T Sign(const T& n) noexcept
     {
         return n >= static_cast<T>(0) ? static_cast<T>(1) : static_cast<T>(-1);
     }
