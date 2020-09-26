63c43896bb57fbf40fc4dbdc5f22350838d4b95f
blAs1N <qazasa123@gmail.com>
2020-09-26 15:04:37 +0900

    Combine code

--- Inc/BSMath/Vector3.h
+++ Inc/BSMath/Vector3.h
@@ -155,8 +155,7 @@ namespace BSMath
 	{
 		const __m128 lhs = _mm_set_ps(0.0f, z, y, x);
 		const __m128 rhs = _mm_set_ps(0.0f, other.z, other.y, other.x);
-		const auto val = _mm_movemask_ps(_mm_cmpeq_ps(lhs, rhs));
-		return val == 15;
+		return _mm_movemask_ps(_mm_cmpeq_ps(lhs, rhs)) == 15;
 	}
 
 	[[nodiscard]] Vector3 Vector3::operator-() const noexcept
