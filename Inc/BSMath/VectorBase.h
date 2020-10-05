97a4b71c04a4bcc771ea969b24fe2e5f72725cc4
blAs1N <qazasa123@gmail.com>
2020-10-05 09:36:18 +0900

    Unwrap namespace in global functions

--- Inc/BSMath/VectorBase.h
+++ Inc/BSMath/VectorBase.h
@@ -1,161 +1,165 @@
 #pragma once
 
 #include "MatrixBase0.h"
+#include "Utility.h"
 
-namespace BSMath::Detail
+namespace BSMath
 {
-	template <class T, size_t L>
-	struct alignas(16) VectorBase : public MatrixBase0<T, 1, L>
+	namespace Detail
 	{
-	public:
-		using Super = MatrixBase0<T, 1, L>;
-
-		const static VectorBase Zero;
-		const static VectorBase One;
-
-	public:
-		using Super::Super;
-
-		template <class U, size_t L2>
-		explicit VectorBase(const VectorBase<U, L2>& other) noexcept
-			: Super()
+		template <class T, size_t L>
+		struct alignas(16) VectorBase : public MatrixBase0<T, 1, L>
 		{
-			std::copy_n(data, L2, other.data);
+		public:
+			using Super = MatrixBase0<T, 1, L>;
+
+			const static VectorBase Zero;
+			const static VectorBase One;
+
+		public:
+			using Super::Super;
+
+			template <class U, size_t L2>
+			explicit VectorBase(const VectorBase<U, L2>& other) noexcept
+				: Super()
+			{
+				std::copy_n(data, L2, other.data);
+			}
+
+			VectorBase(const Super& super) noexcept
+				: Super(super) {}
+
+			void Set(std::initializer_list<T> list) noexcept
+			{
+				std::copy_n(data, L, list.begin());
+			}
+
+			[[nodiscard]] constexpr T GetMin() const noexcept { return Min(*data, *data + L); }
+			[[nodiscard]] constexpr T GetMax() const noexcept { return Max(*data, *data + L); }
+
+			[[nodiscard]] NO_ODR float Length() const noexcept
+			{
+				return Sqrt(LengthSquared());
+			}
+
+			[[nodiscard]] float LengthSquared() const noexcept
+			{
+				return VectorBase<T, L>::Dot(*this, *this);
+			}
+
+			[[nodiscard]] VectorBase GetNormal() const noexcept
+			{
+				VectorBase ret = *this;
+				return ret.Normalize() ? ret : Zero;
+			}
+
+			bool Normalize() noexcept;
+
+			[[nodiscard]] static float Distance(const VectorBase& lhs, const VectorBase& rhs)
+			{
+				return Sqrt(DistanceSquared(lhs, rhs));
+			}
+
+			[[nodiscard]] static float DistanceSquared(const VectorBase& lhs, const VectorBase& rhs)
+			{
+				return VectorBase(lhs - rhs).LengthSquared();
+			}
+
+			[[nodiscard]] constexpr T& operator[](size_t idx) noexcept { return data[idx]; }
+			[[nodiscard]] constexpr T operator[](size_t idx) const noexcept { return data[idx]; }
+
+			[[nodiscard]] VectorBase operator-() const noexcept;
+			VectorBase& operator*=(const VectorBase& other) noexcept;
+			VectorBase& operator/=(const VectorBase& other) noexcept;
+
+			[[nodiscard]] static T Dot(const VectorBase& lhs, const VectorBase& rhs) noexcept;
+		};
+
+		template <class T, size_t L>
+		const VectorBase<T, L> VectorBase<T, L>::Zero;
+
+		template <class T, size_t L>
+		const VectorBase<T, L> VectorBase<T, L>::One(1.0f);
+
+		template <class T, size_t L>
+		bool VectorBase<T, L>::Normalize() noexcept
+		{
+			INVOKE_SIMD(T,
+				const float lengthSquared = LengthSquared();
+				if (IsNearlyZero(lengthSquared))
+					return false;
+
+				const auto vec = LoadRow(data[0]);
+				const auto size = VectorLoad1(InvSqrt(lengthSquared));
+				VectorStore(VectorMultiply(vec, size), data[0]);
+				return true;
+			);
 		}
 
-		VectorBase(const Super& super) noexcept
-			: Super(super) {}
-
-		void Set(std::initializer_list<T> list) noexcept
+		template <class T, size_t L>
+		VectorBase<T, L> VectorBase<T, L>::operator-() const noexcept
 		{
-			std::copy_n(data, L, list.begin());
+			return VectorBase<T, L>::Zero - *this;
 		}
 
-		[[nodiscard]] constexpr T GetMin() const noexcept { return Min(data, data + L); }
-		[[nodiscard]] constexpr T GetMax() const noexcept { return Max(data, data + L); }
-
-		[[nodiscard]] NO_ODR float Length() const noexcept
+		template <class T, size_t L>
+		VectorBase<T, L>& VectorBase<T, L>:: operator*=(const VectorBase<T, L>& other) noexcept
 		{
-			return Sqrt(LengthSquared());
+			INVOKE_SIMD(T,
+				const auto lhs = LoadRow(data[0]);
+				const auto rhs = LoadRow(other.data[0]);
+				VectorStore(VectorMultiply(lhs, rhs), data[0]);
+				return *this;
+			);
 		}
 
-		[[nodiscard]] float LengthSquared() const noexcept
+		template <class T, size_t L>
+		VectorBase<T, L>& VectorBase<T, L>:: operator/=(const VectorBase<T, L>& other) noexcept
 		{
-			return VectorBase<T, L>::Dot(*this, *this);
+			INVOKE_SIMD(T,
+				const auto lhs = LoadRow(data[0]);
+				const auto rhs = LoadRow(other.data[0]);
+				VectorStore(VectorDivide(lhs, rhs), data[0]);
+				return *this;
+			);
 		}
 
-		[[nodiscard]] VectorBase GetNormal() const noexcept
+		template <class T, size_t L>
+		T VectorBase<T, L>::Dot(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
 		{
-			VectorBase ret = *this;
-			return ret.Normalize() ? ret : Zero;
+			auto mul = lhs; mul *= rhs;
+
+			T ret{ 0 };
+			for (size_t i = 0; i < L; ++i)
+				ret += mul.data[0][i];
+			return ret;
 		}
 
-		bool Normalize() noexcept;
+		// Global Operator
 
-		[[nodiscard]] static float Distance(const VectorBase& lhs, const VectorBase& rhs)
+		template <class T, size_t L>
+		[[nodiscard]] NO_ODR VectorBase<T, L> operator*(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
 		{
-			return Sqrt(DistanceSquared(lhs, rhs));
+			return VectorBase<T, L>{ lhs } *= rhs;
 		}
 
-		[[nodiscard]] static float DistanceSquared(const VectorBase& lhs, const VectorBase& rhs)
+		template <class T, size_t L>
+		[[nodiscard]] NO_ODR VectorBase<T, L> operator/(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
 		{
-			return VectorBase(lhs - rhs).LengthSquared();
+			return VectorBase<T, L>{ lhs } *= rhs;
 		}
 
-		[[nodiscard]] constexpr T& operator[](size_t idx) noexcept { return data[idx]; }
-		[[nodiscard]] constexpr T operator[](size_t idx) const noexcept { return data[idx]; }
-
-		[[nodiscard]] VectorBase operator-() const noexcept;
-		VectorBase& operator*=(const VectorBase& other) noexcept;
-		VectorBase& operator/=(const VectorBase& other) noexcept;
-
-		[[nodiscard]] static T Dot(const VectorBase& lhs, const VectorBase& rhs) noexcept;
-	};
-
-	template <class T, size_t L>
-	const VectorBase<T, L> VectorBase<T, L>::Zero;
-
-	template <class T, size_t L>
-	const VectorBase<T, L> VectorBase<T, L>::One(1.0f);
-
-	template <class T, size_t L>
-	bool VectorBase<T, L>::Normalize() noexcept
-	{
-		INVOKE_SIMD(T,
-			const float lengthSquared = LengthSquared();
-			if (IsNearlyZero(lengthSquared))
-				return false;
-			
-			const auto vec = LoadRow(data[0]);
-			const auto size = SIMD::VectorLoad1(InvSqrt(lengthSquared));
-			VectorStore(VectorMultiply(vec, size), data[0]);
-			return true;
-		);
-	}
-
-	template <class T, size_t L>
-	VectorBase<T, L> VectorBase<T, L>::operator-() const noexcept
-	{
-		return VectorBase<T, L>::Zero - *this;
-	}
-
-	template <class T, size_t L>
-	VectorBase<T, L>& VectorBase<T, L>:: operator*=(const VectorBase<T, L>& other) noexcept
-	{
-		INVOKE_SIMD(T,
-			const auto lhs = LoadRow(data[0]);
-			const auto rhs = LoadRow(other.data[0]);
-			VectorStore(VectorMultiply(lhs, rhs), data[0]);
-			return *this;
-		);
-	}
-
-	template <class T, size_t L>
-	VectorBase<T, L>& VectorBase<T, L>:: operator/=(const VectorBase<T, L>& other) noexcept
-	{
-		INVOKE_SIMD(T,
-			const auto lhs = LoadRow(data[0]);
-			const auto rhs = LoadRow(other.data[0]);
-			VectorStore(VectorDivide(lhs, rhs), data[0]);
-			return *this;
-		);
-	}
-
-	template <class T, size_t L>
-	T VectorBase<T, L>::Dot(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
-	{
-		auto mul = lhs; mul *= rhs;
-
-		T ret{ 0 };
-		for (size_t i = 0; i < L; ++i)
-			ret += mul.data[0][i];
-		return ret;
-	}
-
-	// Global Operator
-
-	template <class T, size_t L>
-	[[nodiscard]] NO_ODR VectorBase<T, L> operator*(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
-	{
-		return VectorBase<T, L>{ lhs } *= rhs;
-	}
-
-	template <class T, size_t L>
-	[[nodiscard]] NO_ODR VectorBase<T, L> operator/(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
-	{
-		return VectorBase<T, L>{ lhs } *= rhs;
-	}
-
-	template <class T, size_t L>
-	[[nodiscard]] NO_ODR T operator|(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
-	{
-		return VectorBase<T, L>::Dot(lhs, rhs);
+		template <class T, size_t L>
+		[[nodiscard]] NO_ODR T operator|(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
+		{
+			return VectorBase<T, L>::Dot(lhs, rhs);
+		}
 	}
 
 	// Global Function
 
 	template <class T, size_t L>
-	[[nodiscard]] NO_ODR auto Min(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
+	[[nodiscard]] NO_ODR auto Min(const Detail::VectorBase<T, L>& lhs, const Detail::VectorBase<T, L>& rhs) noexcept
 	{
 		INVOKE_SIMD(T,
 			Vector<T, L> ret;
@@ -165,7 +169,7 @@ namespace BSMath::Detail
 	}
 
 	template <class T, size_t L>
-	[[nodiscard]] NO_ODR auto Max(const VectorBase<T, L>& lhs, const VectorBase<T, L>& rhs) noexcept
+	[[nodiscard]] NO_ODR auto Max(const Detail::VectorBase<T, L>& lhs, const Detail::VectorBase<T, L>& rhs) noexcept
 	{
 		INVOKE_SIMD(T,
 			Vector<T, L> ret;
@@ -175,7 +179,8 @@ namespace BSMath::Detail
 	}
 
 	template <class T, size_t L>
-	[[nodiscard]] NO_ODR auto Clamp(const VectorBase<T, L>& n, const VectorBase<T, L>& min, const VectorBase<T, L>& max) noexcept
+	[[nodiscard]] NO_ODR auto Clamp(const Detail::VectorBase<T, L>& n,
+		const Detail::VectorBase<T, L>& min, const Detail::VectorBase<T, L>& max) noexcept
 	{
 		auto realMin = Min(min, max);
 		auto realMax = Max(min, max);
@@ -183,7 +188,7 @@ namespace BSMath::Detail
 	}
 
 	template <size_t L>
-	[[nodiscard]] NO_ODR VectorBase<int, L> Abs(const VectorBase<int, L>& n) noexcept
+	[[nodiscard]] NO_ODR Detail::VectorBase<int, L> Abs(const Detail::VectorBase<int, L>& n) noexcept
 	{
 		using namespace SIMD::Integer;
 
@@ -195,7 +200,7 @@ namespace BSMath::Detail
 	}
 
 	template <size_t L>
-	[[nodiscard]] NO_ODR VectorBase<float, L> Abs(const VectorBase<float, L>& n) noexcept
+	[[nodiscard]] NO_ODR Detail::VectorBase<float, L> Abs(const Detail::VectorBase<float, L>& n) noexcept
 	{
 		using namespace SIMD::Real;
 		const auto vec = LoadRow(n.data[0]);
@@ -204,7 +209,7 @@ namespace BSMath::Detail
 	}
 
 	template <class T, size_t L>
-	[[nodiscard]] NO_ODR VectorBase<T, L> Sign(const VectorBase<T, L>& n) noexcept
+	[[nodiscard]] NO_ODR Detail::VectorBase<T, L> Sign(const Detail::VectorBase<T, L>& n) noexcept
 	{
 		INVOKE_SIMD(T,
 			const auto vec = LoadRow(n.data[0]);
@@ -215,7 +220,8 @@ namespace BSMath::Detail
 	}
 
 	template <size_t L>
-	[[nodiscard]] NO_ODR bool IsNearlyEqual(const VectorBase<float, L>& lhs, const VectorBase<float, L>& rhs, float tolerance = Epsilon) noexcept
+	[[nodiscard]] NO_ODR bool IsNearlyEqual(const Detail::VectorBase<float, L>& lhs,
+		const Detail::VectorBase<float, L>& rhs, float tolerance = Epsilon) noexcept
 	{
 		using namespace SIMD::Real;
 		const auto epsilon = VectorLoad1(tolerance);
@@ -224,7 +230,7 @@ namespace BSMath::Detail
 	}
 
 	template <size_t L>
-	[[nodiscard]] NO_ODR bool IsNearlyZero(const VectorBase<float, L>& vec, float tolerance = Epsilon) noexcept
+	[[nodiscard]] NO_ODR bool IsNearlyZero(const Detail::VectorBase<float, L>& vec, float tolerance = Epsilon) noexcept
 	{
 		using namespace SIMD::Real;
 		const auto epsilon = VectorLoad1(tolerance);
@@ -233,7 +239,8 @@ namespace BSMath::Detail
 	}
 
 	template <size_t L>
-	[[nodiscard]] NO_ODR VectorBase<float, L> GetRangePct(const VectorBase<float, L>& vec, const VectorBase<float, L>& min, const VectorBase<float, L>& max) noexcept
+	[[nodiscard]] NO_ODR Detail::VectorBase<float, L> GetRangePct(const Detail::VectorBase<float, L>& vec,
+		const Detail::VectorBase<float, L>& min, const Detail::VectorBase<float, L>& max) noexcept
 	{
 		using namespace SIMD::Real;
 		const auto vecSimd = LoadRow(vec.data[0]);
