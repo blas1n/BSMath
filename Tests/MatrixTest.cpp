600c10e10b5e7cf646b540e288e192f8b92c4e20
blAs1N <qazasa123@gmail.com>
2020-10-14 11:41:08 +0900

    Create matrix test file

---
+++ Tests/MatrixTest.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <gtest/gtest.h>
+#include "BSMath/Matrix.h"
+
+namespace BSMath
+{
+	TEST(MatrixTest, Determinant)
+	{
+
+	}
+
+	TEST(MatrixTest, Inverse)
+	{
+
+	}
+
+	TEST(MatrixTest, Transpose)
+	{
+
+	}
+
+	TEST(MatrixTest, Multiply)
+	{
+		
+	}
+}
