7ca9aaf6bc8bcf364acfd3c331ce1c561ff2040d
blAs1N <qazasa123@gmail.com>
2020-10-15 12:07:16 +0900

    Define type

--- Inc/BSMath/Basic.h
+++ Inc/BSMath/Basic.h
@@ -1,4 +1,12 @@
 #pragma once
 
 #define NO_ODR inline
-using byte = unsigned char;
+using uint8 = unsigned char;
+using uint16 = unsigned char;
+using uint32 = unsigned int;
+using uint64 = unsigned long long;
+
+using int8 = char;
+using int16 = short;
+using int32 = int;
+using int64 = long long;
