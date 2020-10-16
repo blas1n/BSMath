9fe7fe510c3c5e62cc605aec32bdb0395d0e5afe
blAs1N <qazasa123@gmail.com>
2020-10-16 11:09:36 +0900

    Add test of matrix's transpose, multiply

--- Tests/MatrixTest.cpp
+++ Tests/MatrixTest.cpp
@@ -46,11 +46,46 @@ namespace BSMath
 
 	TEST(MatrixTest, Transpose)
 	{
+		Matrix<4> lhs
+		{
+			 5.0f,  4.0f, 12.0f,  7.0f,
+			14.0f,  9.0f,  8.0f,  3.0f,
+			 6.0f, 10.0f,  1.0f,  0.0f,
+			11.0f,  6.0f,  3.0f,  8.0f
+		};
 
+		Matrix<4> rhs
+		{
+			 5.0f, 14.0f,  6.0f, 11.0f,
+			 4.0f,  9.0f, 10.0f,  6.0f,
+			12.0f,  8.0f,  1.0f,  3.0f,
+			 7.0f,  3.0f,  0.0f,  8.0f
+		};
+
+		EXPECT_EQ(lhs.GetTranspose(), rhs);
+
+		lhs.Transpose();
+		EXPECT_EQ(lhs.GetTranspose(), rhs.GetTranspose());
 	}
 
 	TEST(MatrixTest, Multiply)
 	{
-		
+		Matrix<4> lhs
+		{
+			 5.0f,  4.0f, 12.0f,  7.0f,
+			14.0f,  9.0f,  8.0f,  3.0f,
+			 6.0f, 10.0f,  1.0f,  0.0f,
+			11.0f,  6.0f,  3.0f,  8.0f
+		};
+
+		Matrix<4> rhs
+		{
+			234.0f, 223.0f,  82.0f, 171.0f,
+			223.0f, 350.0f, 182.0f, 256.0f,
+			 82.0f, 182.0f, 137.0f, 129.0f,
+			171.0f, 256.0f, 129.0f, 230.0f
+		};
+
+		EXPECT_EQ(lhs * lhs.GetTranspose(), rhs);
 	}
 }
