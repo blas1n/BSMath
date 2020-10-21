6df3fe3fde6be7d468d70f867a6a045a9340da4d
blAs1N <qazasa123@gmail.com>
2020-10-21 10:30:13 +0900

    Add test case

--- Tests/QuaternionTest.cpp
+++ Tests/QuaternionTest.cpp
@@ -11,10 +11,12 @@ namespace BSMath
 		Quaternion quat1{ 0.0f, 0.0f, 0.0f, 1.0f };
 		Quaternion quat2{ Quaternion::Identity };
 		Quaternion quat3{ Zeros };
+		Quaternion quat4{ Vector3::Zero, 0.0f };
 
 		EXPECT_EQ(quat0, quat1);
 		EXPECT_EQ(quat0, quat2);
 		EXPECT_EQ(quat0, quat3);
+		EXPECT_EQ(quat0, quat4);
 	}
 
 	TEST(QuaternionTest, Multiply)
