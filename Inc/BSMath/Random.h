96291c7e737276e6655b5cd65b0332862f8f520b
blAs1N <qazasa123@gmail.com>
2020-11-06 19:20:36 +0900

    Refactor random

--- Inc/BSMath/Random.h
+++ Inc/BSMath/Random.h
@@ -6,27 +6,43 @@ namespace BSMath
 {
 	namespace Detail
 	{
-		class RandomBase
+		template <class Distributor>
+		struct HasParam final
 		{
-		protected:
-			static std::random_device randomDevice;
+		private:
+			template <typename C>
+			static std::true_type Test(typename C::param_type*);
+
+			template <typename C>
+			static std::false_type Test(...);
+
+		public:
+			constexpr static bool value = decltype(Test<Distributor>(0))::value;
 		};
 	}
 
+	template <class T, class Engine, class Distributor, bool = Detail::HasParam<Distributor>::value>
+	class Random;
+
 	template <class T, class Engine, class Distributor>
-	class Random final : public Detail::RandomBase
+	class Random<T, Engine, Distributor, true> final
 	{
 	public:
 		using Seed = typename Engine::result_type;
 		using Parameter = typename Distributor::param_type;
 
 	public:
-		Random() : engine(randomDevice) {}
+		Random() : engine(std::random_device{}()), distributor() {}
+		Random(const Parameter& param) : engine(std::random_device{}()), distributor(param) {}
+
+		T operator()() noexcept
+		{
+			return distributor(engine);
+		}
 
 		T operator()(const Parameter& param) noexcept
 		{
-			Distributor distributor{ engine };
-			return distributor(param);
+			return distributor(engine, param);
 		}
 
 		void SetSeed(Seed seed)
@@ -36,6 +52,31 @@ namespace BSMath
 
 	private:
 		Engine engine;
+		Distributor distributor;
+	};
+
+	template <class T, class Engine, class Distributor>
+	class Random<T, Engine, Distributor, false> final
+	{
+	public:
+		using Seed = typename Engine::result_type;
+
+	public:
+		Random() : engine(std::random_device{}()), distributor() {}
+
+		T operator()() noexcept
+		{
+			return distributor(engine);
+		}
+
+		void SetSeed(Seed seed)
+		{
+			engine.seed(seed);
+		}
+
+	private:
+		Engine engine;
+		Distributor distributor;
 	};
 	
 	using UniformIntRandom = Random<int, std::mt19937, std::uniform_int_distribution<int>>;
