4d9044f6c0497c82a4f0ea73a93074ad4b7b541c
blAs1N <qazasa123@gmail.com>
2020-11-06 19:20:53 +0900

    Test random

---
+++ Tests/RandomTest.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+#include "BSMath/Color.h"
+#include "BSMath/Rotator.h"
+#include "BSMath/Vector.h"
+
+// This test only checks whether the build was successful. So, the results are not tested.
+namespace BSMath
+{
+	TEST(Random, Color)
+	{
+		ColorRandom colorRand;
+		(void)colorRand();
+	}
+
+	TEST(Random, Rotator)
+	{
+		RotatorRandom rotatorRand;
+		(void)rotatorRand();
+	}
+
+	TEST(Random, Vector)
+	{
+		Vector2Random vec2Rand;
+		(void)vec2Rand();
+		(void)vec2Rand(Vector2Random::Parameter{ 0.0f, 1.0f });
+
+		IntVector2Random intVec2Rand;
+		(void)intVec2Rand();
+		(void)intVec2Rand(IntVector2Random::Parameter{ 0, 1 });
+
+		Vector3Random vec3Rand;
+		(void)vec3Rand();
+		(void)vec3Rand(Vector3Random::Parameter{ 0.0f, 1.0f });
+
+		IntVector3Random intVec3Rand;
+		(void)intVec3Rand();
+		(void)intVec3Rand(IntVector3Random::Parameter{ 0, 1 });
+
+		Vector4Random vec4Rand;
+		(void)vec4Rand();
+		(void)vec4Rand(Vector4Random::Parameter{ 0.0f, 1.0f });
+
+		IntVector4Random intVec4Rand;
+		(void)intVec4Rand();
+		(void)intVec4Rand(IntVector4Random::Parameter{ 0, 1 });
+	}
+}
