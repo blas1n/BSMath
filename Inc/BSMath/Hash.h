ee49fed5831ace29990d745496d0ad7e04bf139b
blAs1N <qazasa123@gmail.com>
2020-11-06 16:41:27 +0900

    Add hash range function objct

--- Inc/BSMath/Hash.h
+++ Inc/BSMath/Hash.h
@@ -6,6 +6,24 @@
 
 namespace BSMath
 {
+	template <class T, size_t L>
+	struct HashRange
+	{
+		[[nodiscard]] size_t operator()(const T& value) const noexcept
+		{
+			constexpr static auto HashOffsetBasis = sizeof(size_t) == 8 ? 14695981039346656037ull : 2166136261u;
+			constexpr static auto HashPrime = sizeof(size_t) == 8 ? 1099511628211ull : 16777619u;
+
+			size_t val = HashOffsetBasis;
+			for (size_t i = 0; i < L; ++i)
+			{
+				val ^= reinterpret_cast<const unsigned char*>(&value)[i];
+				val *= HashPrime;
+			}
+			return val;
+		}
+	};
+
 	namespace Detail
 	{
 		template <class T>
@@ -33,16 +51,7 @@ namespace BSMath
 		{
 			[[nodiscard]] size_t operator()(const T& value) const noexcept
 			{
-				constexpr static auto HashOffsetBasis = sizeof(size_t) == 8 ? 14695981039346656037ull : 2166136261u;
-				constexpr static auto HashPrime = sizeof(size_t) == 8 ? 1099511628211ull : 16777619u;
-
-				size_t val = HashOffsetBasis;
-				for (size_t i = 0; i < sizeof(T); ++i)
-				{
-					val ^= reinterpret_cast<const unsigned char*>(&value)[i];
-					val *= HashPrime;
-				}
-				return val;
+				return HashRange<T, sizeof(T)>{}(value);
 			}
 		};
 	}
