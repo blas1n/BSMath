5cf6afdc8225e6197b0e678ec8e0486b96661feb
blAs1N <qazasa123@gmail.com>
2021-04-18 14:47:53 +0900

    Set CMAKE_TOOLCHAIN_FILE

--- Scripts/Setup.bat
+++ Scripts/Setup.bat
@@ -2,5 +2,5 @@
 
 pushd %~dp0
 set VCPKG_FEATURE_FLAGS=manifests,registries
-cmake -S .. -B ../Binaries/
+cmake -S .. -B ../Binaries/ -DCMAKE_TOOLCHAIN_FILE=../ThirdParty/vcpkg/scripts/buildsystems/vcpkg.cmake
 popd
--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -6,6 +6,6 @@ DIR=$(pwd)
 cd $(dirname "$0")
 
 VCPKG_FEATURE_FLAGS=manifests,registries
-cmake -S .. -B ../Binaries/
+cmake -S .. -B ../Binaries/ -DCMAKE_TOOLCHAIN_FILE=../ThirdParty/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 cd "${DIR}"
