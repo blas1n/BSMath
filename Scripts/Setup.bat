fc67fa1eddd85655636134afd7caea3e5e5948b2
blAs1N <qazasa123@gmail.com>
2021-04-18 14:59:56 +0900

    Set build type

--- Scripts/Setup.bat
+++ Scripts/Setup.bat
@@ -1,6 +1,20 @@
 @echo off
 
 pushd %~dp0
+
+if "%1%" == "Debug" (
+	set BUILD_TYPE=Debug
+) else if "%1%" == "Develop" (
+	set BUILD_TYPE=RelWithDebInfo
+) else if "%1%" == "Shipping" (
+	set BUILD_TYPE=MinSizeRel
+) else if "%1" == "" (
+	set BUILD_TYPE=RelWithDebInfo
+) else (
+	echo "Unknown build type."
+	exit /b 1
+)
+
 set VCPKG_FEATURE_FLAGS=manifests,registries
-cmake -S .. -B ../Binaries/ -DCMAKE_TOOLCHAIN_FILE=../ThirdParty/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -S .. -B ../Binaries/ -DCMAKE_TOOLCHAIN_FILE=../ThirdParty/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 popd
