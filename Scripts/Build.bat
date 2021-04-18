23805b48ea3ade1334b139ad15a2bd827c45a6a8
blAs1N <qazasa123@gmail.com>
2021-04-18 22:37:16 +0900

    Apply build type in build scripts

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,5 +1,11 @@
 @echo off
 
+if "%1%" == "" (
+	set BUILD_TYPE=Release
+) else (
+	set BUILD_TYPE=%1
+)
+
 pushd %~dp0
-cmake --build ../Binaries
+cmake --build ../Binaries/%BUILD_TYPE%
 popd
--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -5,6 +5,12 @@ set -e
 DIR=$(pwd)
 cd $(dirname "$0")
 
-cmake --build ../Binaries
+if [ $# -ne 1 ] ; then
+	BUILD_TYPE=$1
+else
+	BUILD_TYPE=Release
+fi
+
+cmake --build ../Binaries/${BUILD_TYPE}
 
 cd "${DIR}"
