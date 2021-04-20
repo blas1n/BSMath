beffe70915df20a99a39da3f31710c13ace08460
blAs1N <qazasa123@gmail.com>
2021-04-20 20:27:56 +0900

    Remove debug echo

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -16,7 +16,6 @@ fi
 cd ..
 mkdir -p Binaries/$BUILD_TYPE
 cd Binaries/$BUILD_TYPE
-echo $BUILD_TYPE
 
 cmake ../.. -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -DCMAKE_INSTALL_PREFIX="$(pwd)"/Install -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVCPKG_FEATURE_FLAGS=manifests,registries
 
